04d40e38b25926a6b561cfa08a997e96ae49eef8
GroupD <groupd@groupd-PowerEdge-R610.(none)>
2013-01-19 16:05:12 +0900

    generated myapp.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Groupd::Application.config.session_store :cookie_store, key: '_groupd_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Groupd::Application.config.session_store :active_record_store
